782b6cf81a8ff0bb14b5ef49a981973b3ed4805e
Sonia Tedesco <sonted@alu.edu.gva.es>
2023-12-02 15:44:01 +0100

    programa de actualización

---
+++ src/actualizador.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class actualizador {
+    public static void main(String[] args){
+     System.out.println("Hola");
+     System.out.println("Este es un ejemplo de actualización del repositorio");
+     System.out.println("vamos a añadir cambios.");
+
+    }
+}
