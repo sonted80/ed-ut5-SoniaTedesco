a959afd6f368ec7e772a0664b0aebc6a9b33aa3c
LucasCasRam3 <luccasram3@alu.edu.gva.com>
2023-12-03 19:54:50 +0100

    Modificación 2

--- src/actualizador.java
+++ src/actualizador.java
@@ -3,6 +3,8 @@ public class actualizador {
     public static void main(String[] args){
         System.out.println("Hola, soy tu colaborador efectuando cambios");
         System.out.println("Un placer colaborar contigo");
+        System.out.println("Soy otro colaborador, voy a añadir un nuevo cambio en esta clase :)");
+
 
     }
 }
